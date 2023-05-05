93a075497f45e4502e8c80a7000d0d1963a48128
asaaldurrani <asaaldurrani7@gmail.com>
2023-05-05 05:09:35 -0700

    ok

---
+++ muzzikhan/muzzzi.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const muzzzi = () => {
+  return (
+    <View>
+      <Text>muzzzi</Text>
+    </View>
+  );
+};
+
+export default muzzzi;
+
+const styles = StyleSheet.create({});
