2cdd2cb5ff329a59f18872b14ab21f53ccccab1c
asaaldurrani <asaaldurrani7@gmail.com>
2023-05-05 03:44:47 -0700

    its new

---
+++ muzii.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const muzii = () => {
+  return (
+    <View>
+      <Text>muzii</Text>
+    </View>
+  );
+};
+
+export default muzii;
+
+const styles = StyleSheet.create({});
